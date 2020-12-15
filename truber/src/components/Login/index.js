aa4c4ea87152da50c00268e48f38a5a53e354fff
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-12-15 16:22:25 -0600

    added login jsx

---
+++ truber/src/components/Login/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "../Login/style.css";
+
+
+function Login() {
+    return (
+     
+            <div className="card">
+                <div className="card-body">
+                    <header className="card-title">TrUber</header>
+                    <p class="card-text">We have the truck for you!</p>
+                    <a href="#" className="btn btn-primary">Sign Up!</a>
+                    <a href="#" className="btn btn-primary">Log In!</a>
+                </div>
+            </div>
+      
+    )
+}
+
+export default Login;
