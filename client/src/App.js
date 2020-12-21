8cb76c571beb3c42d51138dc3379238216046133
Laura Rendon <lrendon1220@gmail.com>
2020-12-20 21:48:43 -0600

    sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import {BrowserRouter as Router,Route} from "react-router-dom";
 import Login from "./components/Login";
 import React from "react";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <Login/>
+    <Router>
+      <div className="App">
+    <Route exact path="/" component={Login} />
+    <Route exact path="/SignUp" component={SignUp}/>
+    </div>
+    </Router>
   );
 }
+
 
 export default App;
