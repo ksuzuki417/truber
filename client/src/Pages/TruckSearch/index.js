feaffb4087d463e6bc8cda89bdff3a457766c766
Keita Suzuki <ksuzuki417@gmail.com>
2021-01-02 11:40:29 -0600

    trying to load data from db

--- client/src/Pages/TruckSearch/index.js
+++ client/src/Pages/TruckSearch/index.js
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../TruckSearch/style.css";
+import API from "../../utils/API";
 
-function TruckCard(props) {
+function TruckCard() {
+    const [trucks, setTrucks] = useState([])
+    const [formObject, setFormObject] = useState({})
+
+    useEffect(() =>{
+        loadTrucks()
+    }, [])
+
+    function loadTrucks() {
+        API.getTrucks()
+        .then(res =>
+            setTrucks(res.data)
+            )
+        .catch(err => console.log(err));    
+    };
+
     return(
         <div className="card" id="selector">
-            <div className="img-container">
-                <img alt={props.name} src={props.image} />
-            </div>
-            <div className="content">
-                <ul>
-                    <li>
-                        <strong>Make:</strong> {props.make}
-                    </li>
-                    <li>
-                        <strong>Model:</strong> {props.model}
-                    </li>
-                    <li>
-                        <strong>Location:</strong> {props.location}
-                    </li>
-                    <li>
-                        <strong>Rate:</strong> {props.rate}
-                    </li>
-                </ul>
-                <button type="submit" class="btn btn-primary">Book This Truck!</button>
-            </div>
+            
+            {trucks.map(truck => (
+                <div>
+                <div className="img-container">
+                    <img alt={truck.name} src={truck.image} />
+                </div>
+                <div className="content">
+                    <ul>
+                        <li>
+                            <strong>Make:</strong> {truck.make}
+                        </li>
+                        <li>
+                            <strong>Model:</strong> {truck.model}
+                        </li>
+                        <li>
+                            <strong>Location:</strong> {truck.location}
+                        </li>
+                        <li>
+                            <strong>Rate:</strong> {truck.rate}
+                        </li>
+                    </ul>
+                    <button type="submit" class="btn btn-primary">Book This Truck!</button>
+                </div>
+                </div>
+            ))}
+
         </div>
     )
 }
