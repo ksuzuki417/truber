b065f63118ce71946749bf5a4014ba465062da3c
Analee Guerra <aguerra1508@gmail.com>
2021-01-02 10:34:37 -0600

    added map

--- client/src/Pages/BookingPage/index.js
+++ client/src/Pages/BookingPage/index.js
@@ -1,80 +1,70 @@
-import React from "react";
+import React, { Component } from 'react';
 import Container from "../../components/Container/Container";
 import Wrapper from "../../components/Wrapper/Wrapper"
-import {Client} from "@googlemaps/google-maps-services-js";
+import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-function BookTruck(props) {
+const mapStyles = {
+	width: '50%',
+	height: '50%',
+};
 
-		/*const places = require('places.js');
-		var placesAutocomplete = places({
-		appId: 'plFCXLAE3ZG5',
-		apiKey: '5095b4ad442d5c7294786a6e13299475',
-		container: document.querySelector('#address-input')
-	});*/
-
-/*Google maps/ routes
-const client = new Client({});
-client
-  .elevation({
-    params: {
-      locations: [{ lat: 45, lng: -110 }],
-      key: "asdf",
-    },
-    timeout: 1000, // milliseconds
-  })
-  .then((r) => {
-    console.log(r.data.results[0].elevation);
-  })
-  .catch((e) => {
-    console.log(e.response.data.error_message);
-  });*/
-
-	return(
-		<Wrapper>
+export class MapContainer extends Component {
+  render() {
+    return (
+			<Wrapper>
 			<Container>
 			<div className="card mb-3">
-  <div className="row no-gutters">
-    <div className="col-md-2">
-      <img src="..." className="card-img" alt="..."/>
-    </div>
-    <div className="col-md-6">
-      <div className="card-body">
-        <h5 className="card-title">Owner Info:</h5>
-        <p className="card-text">Make:</p>
-        <p className="card-text">Model:</p>
+	<div className="row no-gutters">
+		<div className="col-md-2">
+			<img src="..." className="card-img" alt="..."/>
+		</div>
+		<div className="col-md-6">
+			<div className="card-body">
+				<h5 className="card-title">Owner Info:</h5>
+				<p className="card-text">Make:</p>
+				<p className="card-text">Model:</p>
 				<p className="card-text">Color:</p>
 				<p className="card-text">Payload:</p>
 				<p className="card-text">Bed Type:</p>
 				<p className="card-text">Number of Seats:</p>
 				<p className="card-text">Rate:</p>
-      </div>
-    </div>
-  </div>
+			</div>
+		</div>
+	</div>
 </div>
 <div className="card mb-3">
-  <div className="row no-gutters">
-    <div className="col-md-4">
-      <div className="card-body">
-        <h5 className="card-title">User Info:</h5>
+	<div className="row no-gutters">
+		<div className="col-md-4">
+			<div className="card-body">
+				<h5 className="card-title">User Info:</h5>
 				<div className="form-group">
-    <label for="inputAddress">Starting Location:</label>
-    <input type="search" className="form-control" id="address-input" placeholder="1234 Main St City, ST 56789"/>
-  </div>
-  <div className="form-row">
-    <div className="form-group col-md-6">
-      <label for="inputCity">Destination Location:</label>
-      <input type="search" className="form-control" id="address-input" placeholder="6789 Park Ln City, ST 12345"/>
-    </div>
-  </div>
-<h1>Map will go here </h1>
-<button type="button" className="btn btn-primary">Book Now</button>
-      </div>
-    </div>
-  </div>
-</div>
-</Container>
-</Wrapper>
-	)
-}
+		<label for="inputAddress">Starting Location:</label>
+		<input type="search" className="form-control" id="address-input" placeholder="1234 Main St City, ST 56789"/>
+	</div>
+	<div className="form-row">
+		<div className="form-group col-md-6">
+			<label for="inputCity">Destination Location:</label>
+			<input type="search" className="form-control" id="address-input" placeholder="6789 Park Ln City, ST 12345"/>
+		</div>
+	</div>
+	<div>
+	<Map google={this.props.google} zoom={14}>
+		<Marker onClick={this.onMarkerClick}
+		name={'Current location'} />
+		 </Map>
+	</div>
+      
+			<button type="button" className="btn btn-primary">Book Now</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	</Container>
+	</Wrapper>
+    );
+  }
+};
 
-export default BookTruck;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAqPz3idSbe0wpPbcnCGWUv4Unj69V7LgQ"
+})(MapContainer);
