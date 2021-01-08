dba7976cb1fbec3294c2043c908c85d1e2040b28
Keita Suzuki <ksuzuki417@gmail.com>
2021-01-07 23:46:47 -0600

    truckSearch page

--- client/src/components/TruckCard/index.js
+++ client/src/components/TruckCard/index.js
@@ -1,21 +1,31 @@
 import React from "react";
-import "./style.css";
+import Container from  "../Container/Container"
 
 // This file exports both the List and ListItem components
 
 export function Card({ children }) {
   return (
-    <div className="card" id="selector">
-        <div className="img-container">
-            <img alt={ children } src={ children } />
-        </div>
-            <div className="content">
-                <ul>
-                    <li>
-                        ({ children })
-                    </li>
-                </ul>
-            </div>
-
-    </div>
+    <div className="card" id="selector">{children}</div>
   )}
+
+export function CardItem({
+    image,
+    make,
+    model,
+    location,
+    rate
+}) {
+    return (
+        <ul>
+        <li className="content">
+            <Container>
+            <img src="..." className="card-img" alt="..."/>
+            <strong>Make:</strong> {make}
+            <strong>Model:</strong> {model}
+            <strong>Location:</strong> {location}
+            <strong>Rate:</strong> {rate}
+            </Container>
+        </li>
+        </ul>
+    )
+}
